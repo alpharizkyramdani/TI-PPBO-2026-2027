1ce3387f12f1720b3a9178291bb28bb61f79874c
alpharizkyramdani <alpharizkyramdani@gmail.com>
2026-09-08 11:39:10 +0700

    Latihan nomor 3

--- src/BiodataSaya.java
+++ src/BiodataSaya.java
@@ -1,11 +1,23 @@
 public class BiodataSaya {
     public static  void main(String[] args) {
+
+        // Deklarasi variabel data pribadi
+        int umur = 19;
+        double tinggiBadan = 171.5;
+        char inisial = 'A';
+        boolean mahasiswaAktif = true;
+
         // Isi Biodata Saya
-        System.out.print("Nama: MHD.Alpharizky Ramdani - ");
-        System.out.print("Nim: 2025573010078 - ");
-        System.out.print("Program Studi: Teknik Informatika");
+        System.out.println("Nama: MHD.Alpharizky Ramdani - ");
+        System.out.println("Nim: 2025573010078 - ");
+        System.out.println("Program Studi: Teknik Informatika");
         /*
         ini dia isi dari Biodata saya
          */
+
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi Badan: " + tinggiBadan + "cm");
+        System.out.println("Inisial: " + inisial);
+        System.out.println("Mahasiswa Aktif: " + mahasiswaAktif);
     }
 }
