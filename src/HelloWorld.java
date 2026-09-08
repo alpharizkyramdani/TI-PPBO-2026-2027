6bc2ea8a2e780581cfa4e6d0f9b1df69d4eee758
alpharizkyramdani <alpharizkyramdani@gmail.com>
2026-09-08 09:34:48 +0700

    Komentar Kode

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Nama: ");
-        System.out.print("Alpha");
-        System.out.println();
-        System.out.println("Halo Semuanya");
+        // program menampilkan biodata sederhana
+        System.out.println("Nama: Alpha");
+        /* Baris di bawah ini
+           mencetak Nim mahasiswa */
+        System.out.println("NIM: 2025572010078");
     }
 }
