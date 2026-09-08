eaad9ee54fb7dd269b91fc440aa11f0c3b5d2604
alpharizkyramdani <alpharizkyramdani@gmail.com>
2026-09-08 11:05:07 +0700

    Casting(Konversi Tipe Data)

--- src/OperatorDemo.java
+++ src/OperatorDemo.java
@@ -1,11 +1,8 @@
 public class OperatorDemo {
     public static void main(String[] args) {
-        int a = 20, b = 25;
-
-        System.out.println("Jumlah: " + (a + b));
-        System.out.println("Selisih: " + (a - b));
-        System.out.println("Kali: " + (a * b));
-        System.out.println("Bagi: " + (a / b));
-        System.out.println("Sisa Bagi: " + (a % b));
+        int nilaiUjian = 78;
+        boolean hadir = true;
+        System.out.println("Lulus (>=75): " + (nilaiUjian >= 75));
+        System.out.println("Lulus DAN hadir: " + (nilaiUjian >= 75 && hadir));
     }
 }
