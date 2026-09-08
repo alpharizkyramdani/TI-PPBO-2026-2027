5db4d7747cf21f8e8c416d751db7687919bae8a0
alpharizkyramdani <alpharizkyramdani@gmail.com>
2026-09-08 09:50:19 +0700

    Deklarasi Variabel dan Tipe Data Primitif

---
+++ src/VariabelDemo.java
@@ -0,0 +1,15 @@
+public class VariabelDemo {
+    public static void main (String[] agrs) {
+        String nama = "Alpha";
+        int umur = 19;
+        double tinggi = 171.5;
+        char golDarah = 'o';
+        boolean mahasiswaAktif = true;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi: " + tinggi);
+        System.out.println("Golongan Darah: " + golDarah);
+        System.out.println("Aktif: " + mahasiswaAktif);
+    }
+}
