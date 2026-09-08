3a94995a237754ad888f11d272e1674ab45a3f58
alpharizkyramdani <alpharizkyramdani@gmail.com>
2026-09-08 11:28:41 +0700

    Latihan nomor 2

--- src/BiodataSaya.java
+++ src/BiodataSaya.java
@@ -1,9 +1,9 @@
 public class BiodataSaya {
     public static  void main(String[] args) {
         // Isi Biodata Saya
-        System.out.println("Nama: MHD.Alpharizky Ramdani");
-        System.out.println("Nim: 2025573010078");
-        System.out.println("Program Studi: Teknik Informatika");
+        System.out.print("Nama: MHD.Alpharizky Ramdani - ");
+        System.out.print("Nim: 2025573010078 - ");
+        System.out.print("Program Studi: Teknik Informatika");
         /*
         ini dia isi dari Biodata saya
          */
