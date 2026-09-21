4373ec8e19d2f99f1890d692beb2cd888aa26046
alpharizkyramdani <alpharizkyramdani@gmail.com>
2026-09-21 21:02:52 +0700

    Tambahkan latihan dan tugas Praktikum 2

---
+++ praktikum-2/Latihan 1/BiodataSaya.java
@@ -0,0 +1,11 @@
+﻿public class BiodataSaya {
+    public static  void main(String[] args) {
+        // Isi Biodata Saya
+        System.out.println("Nama: MHD.Alpharizky Ramdani");
+        System.out.println("Nim: 2025573010078");
+        System.out.println("Program Studi: Teknik Informatika");
+        /*
+        ini dia isi dari Biodata saya
+         */
+    }
+}
---
+++ praktikum-2/Latihan 2/BiodataSaya.java
@@ -0,0 +1,11 @@
+﻿public class BiodataSaya {
+    public static  void main(String[] args) {
+        // Isi Biodata Saya
+        System.out.print("Nama: MHD.Alpharizky Ramdani - ");
+        System.out.print("Nim: 2025573010078 - ");
+        System.out.print("Program Studi: Teknik Informatika");
+        /*
+        ini dia isi dari Biodata saya
+         */
+    }
+}
